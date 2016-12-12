8b22d850c8ebf89aee0ac33782ae1f9c1deed8db
Bartolomeu Bonze Jeremy <jeremy.b.bonze@gmail.com>
2016-12-12 02:01:52 +0100

    [JS] Display several tweets

--- js/tweetmap.js
+++ js/tweetmap.js
@@ -1,5 +1,6 @@
 var map;
 var content;
+var geocoder;
 
 function initMap() {
 
@@ -10,39 +11,24 @@ function initMap() {
         },
         zoom: 11
     });
+    geocoder = new google.maps.Geocoder();
+    searchTweets("", "40.6976701,-74.2598661,10km");
 
-    var uri = encodeURIComponent("https://twitter.com/USER/status/807749687944089602");
-    console.log(uri);
-    ajaxGet("http://localhost:8888/tweetMap/php/tweetdisplay.php?url=" + uri, function (response) {
-        var embedTweet = JSON.parse(response);
-        console.log(embedTweet);
-        content = embedTweet.html;
-
-        var infowindow = new google.maps.InfoWindow({
-            content: content
-        });
-
-        var marker = new google.maps.Marker({
-            position: new google.maps.LatLng(40.6976701, -74.2598661),
-            map: map,
-            title: 'Tweet'
-        });
-        marker.addListener('click', function () {
-            infowindow.open(map, marker);
-        });
-
-        google.maps.event.addListener(infowindow, 'domready', function () {
-            ! function (d, s, id) {
-                var js, fjs = d.getElementsByTagName(s)[0];
-                if (!d.getElementById(id)) {
-                    js = d.createElement(s);
-                    js.id = id;
-                    js.src = "//platform.twitter.com/widgets.js";
-                    fjs.parentNode.insertBefore(js, fjs);
-                }
-            }(document, "script", "twitter-widgets");
-        });
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(40.6976701, -74.2598661),
+        map: map,
+        title: 'Tweet'
     });
+
+    var marker2 = new google.maps.Marker({
+        position: new google.maps.LatLng(40.688559, -74.206308),
+        map: map,
+        title: 'Tweet'
+    });
+
+    makeInfowindow("807749687944089602", marker);
+    makeInfowindow("808078161774776320", marker2);
+
 }
 
 
@@ -51,17 +37,62 @@ var tweetArray = []
 function searchTweets(query, geocode) {
     // Dont forgot radius for geocode
     var url = "http://localhost:8888/tweetMap/php/searchtweet.php?q=" + query + "&geocode=" + geocode;
-    console.log(url);
     ajaxGet(url, function (response) {
         var tweets = JSON.parse(response).statuses;
-        console.log(tweets);
+        //console.log(tweets);
         tweets.forEach(function (tweet) {
             if (tweet.geo != null)
                 console.log('geo' + tweet.geo);
-            else if (tweet.user.location != null)
-                console.log(tweet.user.location);
+            else if (tweet.user.location != null) {
+                //geocodeAddress(geocoder, map, tweet.user.location);
+            }
+            //console.log(tweet.user.location);
         });
     });
 }
 
-searchTweets("psg", "");
+function makeInfowindow(tweetId, marker) {
+    var url = "http://localhost:8888/tweetMap/php/tweetdisplay.php?url=";
+    var uri = encodeURIComponent("https://twitter.com/user/status/" + tweetId);
+    ajaxGet(url + uri, function (response) {
+        var embedTweet = JSON.parse(response);
+        content = embedTweet.html;
+
+        var infowindow = new google.maps.InfoWindow({
+            content: content
+        });
+
+        marker.addListener('click', function () {
+            infowindow.close();
+            infowindow.open(map, marker);
+        });
+
+        google.maps.event.addListener(infowindow, 'domready', function () {
+            ! function (d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                js = d.createElement(s);
+                js.id = id;
+                js.src = "//platform.twitter.com/widgets.js";
+                fjs.parentNode.insertBefore(js, fjs);
+            }
+            (document, "script", "twitter-widgets");
+        });
+    });
+}
+
+
+function geocodeAddress(geocoder, resultsMap, place) {
+    var address = place;
+    geocoder.geocode({
+        'address': address
+    }, function (results, status) {
+        if (status === google.maps.GeocoderStatus.OK) {
+            var marker = new google.maps.Marker({
+                map: resultsMap,
+                position: results[0].geometry.location
+            });
+        } else {
+            console.log('Geocode was not successful for the following reason: ' + status);
+        }
+    });
+}
